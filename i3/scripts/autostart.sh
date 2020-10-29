f3602bb41ae5a0f2362b8d26a12bb2e6ce798c04
YeloHawPi <w.yelohawpi@gmail.com>
2020-10-29 14:00:10 +1000

    add files

---
+++ i3/scripts/autostart.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Keyboard layout
+setxkbmap -layout us,ru
+setxkbmap -option 'grp:alt_shift_toggle'
+
+# Wallpapers
+feh --bg-scale ~/Изображения/Wallpapers/Japan_ice_landscapes_nature_winter_snow_trees_bridges_1920x1080.jpg
